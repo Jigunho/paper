ae78fd2f16aa4afa8fe35db8875eb6e8a2be9378
jigunho <jce0831@gmail.com>
2020-04-15 12:25:25 +0900

    fpminer 완성

--- 0407_split.js
+++ 0407_split.js
@@ -45,8 +45,8 @@ for (let i = 0; i < u_lines.length; i++) {
 let tmp_object = _.groupBy(log_prev_ary, 'object_id');
 for (let user_id in tmp_object) {
   let arr = tmp_object[user_id];
-  for (let i = 0 ; i < arr.length ; i ++) {
-    if (i%2===0) {
+  for (let i = 0; i < arr.length; i++) {
+    if (i % 2 === 0) {
       log_ary.push(arr[i]);
     }
   }
@@ -126,23 +126,29 @@ for (let area_id in area_median_result) {
   }
 
 }
-// for (let a_g_id in grid_result_intersect) {
-//   let grid_result = grid_result_intersect[a_g_id];
-//   // console.log(JSON.stringify(grid_result));
-//   fs.appendFileSync('./0407_grid_result_small.txt', `${JSON.stringify(grid_result)},\n`);
-//   fs.appendFileSync('./0407_grid_result_big.txt', `${JSON.stringify(grid_result)},\n`);
+for (let a_g_id in grid_result_intersect) {
+  let grid_result = grid_result_intersect[a_g_id];
+  // console.log(`intersect ${JSON.stringify(grid_result)}`);
+  // fs.appendFileSync('./0407_grid_result_small.txt', `${JSON.stringify(grid_result)},\n`);
+  // fs.appendFileSync('./0407_grid_result_big.txt', `${JSON.stringify(grid_result)},\n`);
 
-// }
-// for (let a_g_id in grid_result_small) {
-//   let grid_result = grid_result_small[a_g_id];
-//   fs.appendFileSync('./0407_grid_result_small.txt', `${JSON.stringify(grid_result)},\n`);
+}
+for (let a_g_id in grid_result_small) {
+  let grid_result = grid_result_small[a_g_id];
+  // console.log(`small ${JSON.stringify(grid_result)}`);
 
-// }
-// for (let a_g_id in grid_result_big) {
-//   let grid_result = grid_result_big[a_g_id];
-//   fs.appendFileSync('./0407_grid_result_big.txt', `${JSON.stringify(grid_result)},\n`);
+  // fs.appendFileSync('./0407_grid_result_small.txt', `${JSON.stringify(grid_result)},\n`);
 
-// }
+}
+for (let a_g_id in grid_result_big) {
+  let grid_result = grid_result_big[a_g_id];
+  // console.log(`big ${JSON.stringify(grid_result)}`);
+
+  // fs.appendFileSync('./0407_grid_result_big.txt', `${JSON.stringify(grid_result)},\n`);
+
+}
+
+let user_movement_logs = {}; // 0414에 결과를 확인하기 위하여 user별
 
 const print_target_id = `199`;
 // let target_ids = ['141', '199', '396', '448','497', '763', '765', '892', '1049', '1074', '1164', '1199', '1210', '1278', '1403', '1439', '1453']; // 차량
@@ -265,10 +271,10 @@ for (let target_id in targets_logs) {
         camera_screen_ary[y][x].static = 1;
       }
     }
-    if (target_id === print_target_id) {
-      let obj = { x: start_x, y: start_y, w: static_video_x, h: static_video_y }
-      print_static_grids.push(obj)
-    }
+    // if (target_id === print_target_id) {
+    //   let obj = { x: start_x, y: start_y, w: static_video_x, h: static_video_y }
+    //   print_static_grids.push(obj)
+    // }
 
     // console.log(`${user_static_ids[i]} start_x : ${start_x}, end_x : ${start_x + static_video_x} start_y : ${start_y}, end_y : ${start_y + static_video_y}`);
 
@@ -318,6 +324,8 @@ for (let target_id in targets_logs) {
 
 
 
+  // 유저별 통계 및 리스트에 집중
+
   let user_adaptive_grids = [];
 
   for (let j = 1; j < arr.length; j++) {
@@ -335,18 +343,21 @@ for (let target_id in targets_logs) {
     let mean_width = (arr[j].width + arr[j - 1].width) / 2;
     let mean_height = (arr[j].height + arr[j - 1].height) / 2;
 
-    if (target_id === print_target_id) {
+    // if (target_id === print_target_id) {
 
-      let obj = { x: mean_x - mean_width / 2, y: mean_y - mean_height / 2, w: mean_width, h: mean_height };
-      print_reals.push(obj)
-    }
+    //   let obj = { x: mean_x - mean_width / 2, y: mean_y - mean_height / 2, w: mean_width, h: mean_height };
+    //   print_reals.push(obj)
+    // }
 
 
 
     // const target_split = { 101: 1, 102: 1, 103: 1, 104: 2, 201: 1, 202: 1, 203: 2, 204: 2, 301: 0, 302: 2, 303: 2, 304: 1, 401: 1, 402: 2, 403: 1, 404: 1 };
 
     let u_area_result = user_area_result[arr[j].area_id]
-
+    if (!u_area_result) {
+      continue;
+    }
+    // console.log(`${arr[j].area_id} - ${u_area_result}`)
     let r = -1;
     let g_info = null;
     let g_size = -1;
@@ -371,14 +382,13 @@ for (let target_id in targets_logs) {
 
     }
 
-    if (r == -1 || g_size === -1 || g_size === undefined) {
-
-      if (target_id === print_target_id)
-        console.log(`grid_id:${r}, x,y(${mean_x},${mean_y}) grid_size:${g_size})`);
-      // break;
+    if (r == -1 || g_size === -1 || g_size === undefined || g_info === null) {
+      // if (target_id === print_target_id)
+      // console.log(`grid_id:${r}, x,y(${mean_x},${mean_y}) grid_size:${g_size})`);
+      continue
     } else {
       // g_size = g_size.grid_size;
-      // console.log(`gsize: ${JSON.stringify(g_size)}`)
+      // console.log(`gsize: ${JSON.stringify(r)}`)
       // console.log('grid math success')
       // user_adaptives.push(r);
 
@@ -386,8 +396,16 @@ for (let target_id in targets_logs) {
 
 
     }
+
+    if (!user_movement_logs[target_id]) {
+      let user_movement_log = { direction, area_id: arr[j].area_id, grid_id: r }
+      user_movement_logs[target_id] = [user_movement_log]
+    } else {
+      user_movement_logs[target_id].push({ direction, area_id: arr[j].area_id, grid_id: r })
+    }
+
+
   }
-  // console.log(`${target_id} adaptive: ${JSON.stringify(user_adaptive_grids)}`);
 
 
   let ccc = 0;
@@ -400,9 +418,9 @@ for (let target_id in targets_logs) {
     } else {
       dup_ids.push(user_adaptive_grids[g].id);
 
-      if (target_id === print_target_id) {
-        print_adaptive_grids.push(user_adaptive_grids[g])
-      }
+      // if (target_id === print_target_id) {
+      //   print_adaptive_grids.push(user_adaptive_grids[g])
+      // }
 
     }
     let a_start_x = Math.floor(user_adaptive_grids[g].x);
@@ -419,13 +437,13 @@ for (let target_id in targets_logs) {
 
   }
 
-  if (target_id === print_target_id) {
-    console.log(`target id [${target_id}] - ${dup_ids}`)
-    console.log(`target id [${target_id}] - ${user_static_ids}`)
+  // if (target_id === print_target_id) {
+  //   console.log(`target id [${target_id}] - ${dup_ids}`)
+  //   console.log(`target id [${target_id}] - ${user_static_ids}`)
 
-    console.log(`target id [${target_id}] - ${ccc} ${video_y * video_x}`)
+  //   console.log(`target id [${target_id}] - ${ccc} ${video_y * video_x}`)
 
-  }
+  // }
 
   // real 수 세기
   let real_cnt = 0;
@@ -490,19 +508,236 @@ for (let target_id in targets_logs) {
 
 }
 
-for (let i = 0; i < excel_logs1.length; i++) {
-  fs.appendFileSync(`./grid_description/${file_name}_${split_m}.txt`, `${excel_logs1[i]}\n`)
-}
-for (let i = 0; i < excel_logs2.length; i++) {
-  fs.appendFileSync(`./grid_description/${file_name}_${split_m}.txt`, `${excel_logs2[i]}\n`)
+// for (let i = 0; i < excel_logs1.length; i++) {
+//   fs.appendFileSync(`./grid_description/${file_name}_${split_m}.txt`, `${excel_logs1[i]}\n`)
+// }
+// for (let i = 0; i < excel_logs2.length; i++) {
+//   fs.appendFileSync(`./grid_description/${file_name}_${split_m}.txt`, `${excel_logs2[i]}\n`)
+// }
+
+// for (let i = 0; i < print_adaptive_grids.length; i++) {
+//   fs.appendFileSync(`./grid_description/${file_name}_adaptive.txt`, `${JSON.stringify(print_adaptive_grids[i])},\n`)
+// }
+// for (let i = 0; i < print_reals.length; i++) {
+//   fs.appendFileSync(`./grid_description/${file_name}_reals.txt`, `${JSON.stringify(print_reals[i])},\n`)
+// }
+// for (let i = 0; i < print_static_grids.length; i++) {
+//   fs.appendFileSync(`./grid_description/${file_name}_statics.txt`, `${JSON.stringify(print_static_grids[i])},\n`)
+// }
+
+
+// 0414 fpminer 에 넣을 user_id 유저별 영역트랜잭션, 가변그리드 트랜잭션, 영역 방향 트랜잭션 을 담은 결과 데이터
+
+let user_movement_result = []
+
+for (let user_id in user_movement_logs) {
+  let user_arr = user_movement_logs[user_id];
+  let area_groups = _.groupBy(user_arr, 'area_id');
+  let area_direction_mean_result = {};
+  let area_ids = []
+  let area_directions = [];
+  let a_list = []
+  for (let area_id in area_groups) {
+    let area_logs = area_groups[area_id];
+    let directions = [];
+    for (let i = 0; i < area_logs.length; i++) {
+      directions.push(area_logs[i].direction);
+
+    }
+    area_ids.push(area_id);
+    a_list.push({ area_id, direction: mathjs.mean(directions) });
+    area_directions.push(mathjs.mean(directions));
+    // area_direction_mean_result[area_id] = Math.mean(directions);
+  }
+  let grid_lists = [];
+  for (let g = 0; g < user_arr.length; g++) {
+    grid_lists.push(user_arr[g].grid_id);
+  }
+  grid_lists = [...new Set(grid_lists)]
+  let o = { user_id, grid_lists, area_list: a_list };
+  user_movement_result.push(o);
+  // fs.appendFileSync('./user_result.txt',`${JSON.stringify(o)}\n`);
 }
 
-for (let i = 0; i < print_adaptive_grids.length; i++) {
-  fs.appendFileSync(`./grid_description/${file_name}_adaptive.txt`, `${JSON.stringify(print_adaptive_grids[i])},\n`)
+
+////////// 0414_fpminer copy & paste ////////////////
+
+let threshold = 0.3;
+
+
+
+// console.log(arr3);
+
+const main = () => {
+
+  for (let i = 0; i < user_movement_result.length; i++) {
+    let g_list = user_movement_result[i].grid_lists
+    // console.log(g_list)
+    g_list.sort(function (a, b) {
+      return a - b;
+    })
+    user_movement_result[i].grid_str = g_list.join('@');
+
+    let a_list = user_movement_result[i].area_list;
+    let new_a_list = [];
+    for (let a = 0; a < a_list.length; a++) {
+      a_list[a].direction_id = getDirectionId(a_list[a].direction)
+    }
+    user_movement_result[i].area_list = a_list;
+
+  }
+
+
+  // for (let i = 0; i < user_movement_result.length; i++) {
+  //   console.log(JSON.stringify(user_movement_result[i]))
+  // }
+
+  let group = {}; // area id와 area 방향이 같은 list
+  for (let i = 0; i < user_movement_result.length; i++) {
+    let a_list = user_movement_result[i].area_list;
+    a_list.sort(function (a, b) {
+      return a.id - b.id;
+    })
+    let a_ary = [];
+    for (let a = 0; a < a_list.length; a++) {
+      a_ary.push(`${a_list[a].area_id}_${a_list[a].direction_id}`);
+    }
+    let a_str = a_ary.join('@');
+    if (!group[a_str]) {
+      group[a_str] = [user_movement_result[i]];
+    } else {
+      group[a_str].push(user_movement_result[i]);
+    }
+  }
+  // console.log(Object.keys(group))
+  // for (let a_group in group) {
+  //   if (group[a_group].length > 1) {
+  //     console.log(`${a_group} - ${group[a_group].length}`)
+  //     console.log(`${a_group} : ${JSON.stringify(group[a_group])}`)
+  //   }
+  // }
+  let merge_grid_list = {};
+  for (let a_str in group) {
+    let a_str_list = group[a_str]; // 같은 영역, 같은 방향을 가진 객체들
+    if (!(a_str_list.length > 1)) {
+      continue;
+    }
+
+    // console.log(`${a_str_list.length} - ${JSON.stringify(a_str_list)}`)
+    
+    let group_grid_list = _.groupBy(a_str_list, 'grid_str');
+    let grid_list = Object.keys(group_grid_list);
+    // console.log(`a list 같고 d id list 같고 str 같은 애들 갯수 `);
+    // for (let grid_id_list in group_grid_list) {
+      // console.log(`count: ${ JSON.stringify(group_grid_list[grid_id_list])}`)
+    //   console.log(`${grid_id_list}: ${group_grid_list[grid_id_list].length}`)
+    // }
+    // for (let g_id1 in group_grid_list) {
+    //   for (let g_id2 in group_grid_list) {
+    //   }
+    // }
+    for (let i = 0; i < grid_list.length; i++) {
+      for (let j = 0; j < grid_list.length; j++) {
+        if (i === j) {
+          continue;
+        } else {
+          // 매칭 해보면서 , 조건 같으면
+          let user_grid_list1 = grid_list[i].split('@')
+          let user_grid_list2 = grid_list[j].split('@');
+          let concat_list = user_grid_list1.concat(user_grid_list2);
+          let dup_removed_concat_list = [...new Set(concat_list)];
+          let intersect_list = user_grid_list1.filter(v => user_grid_list2.includes(v))
+          // console.log(`user1 - ${user_grid_list1}`);
+          // console.log('---------------')
+          // console.log(`user2 - ${user_grid_list2}`);
+          // console.log('---------------')
+          // console.log(`dup removed - ${intersect_list}`);
+
+          
+          let ratio = intersect_list.length / mathjs.min([user_grid_list1.length, user_grid_list2.length]);
+          console.log(`ratio: ${ratio} dup len: ${intersect_list.length}, i len: ${user_grid_list1.length}, j len: ${user_grid_list2.length}`)
+          if (ratio > threshold) {
+            // console.log(`${ratio} , ${dup_removed_concat_list}`);
+            let merge_grid = dup_removed_concat_list.join('@');
+            merge_grid_list[grid_list[i]] = merge_grid;
+            merge_grid_list[grid_list[j]] = merge_grid;
+            console.log(`1단계 병합됨`)
+          } else {
+            console.log(`1단계 병합되지 않음`);
+          }
+
+        }
+      }
+    }
+
+
+    let signal = true
+    while(signal) {
+
+      let cnt = 0;
+
+      for (let merged_id1 in merge_grid_list) {
+        let merged_list1 = merge_grid_list[merged_id1].split('@');
+        for (let merged_id2 in merge_grid_list) {
+          let merged_list2 = merge_grid_list[merged_id2].split('@');
+          
+          if (merge_grid_list[merged_id2] === merge_grid_list[merged_id1]) {
+            continue;
+          }
+          
+          let concat_list = merged_list1.concat(merged_list2);
+          let dup_removed_concat_list = [...new Set(concat_list)];
+          let intersect_list = merged_list1.filter(v => merged_list2.includes(v))
+
+          let ratio = intersect_list.length / mathjs.min([merged_list1.length, merged_list2.length]);
+          // console.log(`ratio: ${ratio} dup len: ${intersect_list.length}, i len: ${user_grid_list1.length}, j len: ${user_grid_list2.length}`)
+          if (ratio > threshold) {
+            // console.log(`${ratio} , ${dup_removed_concat_list}`);
+            let merge_grid = dup_removed_concat_list.join('@');
+            merge_grid_list[merged_id1] = merge_grid;
+            merge_grid_list[merged_id2] = merge_grid;
+            // delete[merged_id1];
+            // delete[merged_id2]
+            console.log(merge_grid)
+            console.log(`2단계 병합됨`);
+            cnt += 1;
+            
+          } else {
+            console.log(`2단계 병합되지 않음`);
+            // signal = false;
+          }
+
+        }
+      }
+      if (cnt === 0) {
+        console.log(`while종료`)
+        break;
+      }
+    }
+    
+    // a_list d_list 같은 애들을 병합조건으로 수행하였을때 최종적으로 합쳐진 id 갯수가 몇개인지 확인
+    let merged_group = {};
+    for (let merged_id in merge_grid_list) {
+      // console.log(`${merged_id} -> ${merge_grid_list[merged_id]}`)
+      if (!merged_group[merge_grid_list[merged_id]]) {
+        merged_group[merge_grid_list[merged_id]] = [merged_id]
+      } else {
+        merged_group[merge_grid_list[merged_id]].push(merged_id)
+      }
+    }
+    for (let id in merged_group) {
+      console.log(`${id} !!!!!!!! ${merged_group[id].length}`)
+    }
+
+    // for (let i = 0; i < a_str_list.length; i++) {
+    //   if (merge_grid_list[a_str_list].grid_str) {
+
+    //   }
+    // }
+  }
 }
-for (let i = 0; i < print_reals.length; i++) {
-  fs.appendFileSync(`./grid_description/${file_name}_reals.txt`, `${JSON.stringify(print_reals[i])},\n`)
+const getDirectionId = (direction) => {
+  return Math.floor(direction / 90)
 }
-for (let i = 0; i < print_static_grids.length; i++) {
-  fs.appendFileSync(`./grid_description/${file_name}_statics.txt`, `${JSON.stringify(print_static_grids[i])},\n`)
-}
+
+main();
---
+++ 0414_fpminer.js
@@ -0,0 +1,102 @@
+const _ = require('lodash');
+const fs = require('fs');
+// let result = [{ user_id: 4, grid_list: ['404-01', '404-0001'], area_list: [{ id: '101', direction: 30 }, { id: '102', direction: 50 }] }];
+let threshold = 0.3;
+
+
+let lines = fs.readFileSync('./user_result.txt').toString().split('\n');
+let result = []
+for (let i = 0 ; i < lines.length ; i ++) {
+  result.push(JSON.parse(JSON.stringify(lines[i])))
+}
+console.log(result.length);
+
+
+// console.log(arr3);
+
+const main = () => {
+
+  for (let i = 0; i < result.length; i++) {
+    let g_list = result[i].grid_lists
+    console.log(g_list)
+    // g_list.sort(function (a, b) {
+    //   return a - b;
+    // })
+    // result[i].grid_str = g_list.join('@');
+  
+    // let a_list = result[i].area_list;
+    // for (let a = 0; a < a_list.length; a++) {
+    //   a_list[a].direction_id = getDirectionId[a_list[a].direction];
+    // }
+    // result[i].area_list = a_list;
+  
+  }
+  
+
+  for (let i = 0; i < result.length; i++) {
+    console.log(JSON.stringify(result[i]))
+  }
+
+  // let group = {}; // area id와 area 방향이 같은 list
+  // for (let i = 0; i < result.length; i++) {
+  //   let a_list = result[i].area_list;
+  //   a_list.sort(function (a, b) {
+  //     return a.id - b.id;
+  //   })
+  //   let a_ary = [];
+  //   for (let a = 0; a < a_list.length; a++) {
+  //     a_ary.push(`${a_list[a].id}_${a_list[a].direction_id}`);
+  //   }
+  //   let a_str = a_ary.join('@');
+  //   if (!group(a_str)) {
+  //     group[a_str] = [result[i]];
+  //   } else {
+  //     group[a_str].push(result[i]);
+  //   }
+  // }
+  
+  // let merge_grid_list = {};
+  // for (let a_str in group) {
+  //   let a_str_list = group[a_str]; // 같은 영역, 같은 방향을 가진 객체들
+  
+  //   let group_grid_list = _.groupBy(a_str_list, 'grid_str');
+  //   let grid_list = Object.keys(group_grid_list);
+  //   for (let i = 0; i < grid_list.length; i++) {
+  //     for (let j = 0; j < grid_list.length; j++) {
+  //       if (i === j) {
+  //         continue;
+  //       } else {
+  //         // 매칭 해보면서 , 조건 같으면
+  //         let user_grid_list1 = grid_list[i].grid_str.split('@')
+  //         let user_grid_list2 = grid_list[i].grid_str.split('@');
+  //         let concat_list = user_grid_list1.concat(user_grid_list2);
+  //         let dup_removed_concat_list = [...new Set(concat_list)];
+
+  //         let ratio = dup_removed_concat_list.length / Math.min([user_grid_list1.length, user_grid_list2.length]);
+
+  //         if (ratio > threshold) {
+  //           console.log(`${ratio} , ${dup_removed_concat_list}`);
+  //           let merge_grid = dup_removed_concat_list.join('@');
+  //           merge_grid_list[grid_list[i].grid_str] = merge_grid;
+  //           merge_grid_list[grid_list[j].grid_str] = merge_grid;
+  
+  //         }
+          
+  //       }
+  //     }
+  //   }
+  
+  //   let merged_group = {};
+  
+  //   for (let i = 0; i < a_str_list.length; i++) {
+  //     if (merge_grid_list[a_str_list].grid_str) {
+  
+  //     }
+  //   }
+  // }
+}
+const getDirectionId = (direction) => {
+  return Math.floor(direction / 90)
+}
+
+main();
